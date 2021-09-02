1e8a584c82c395f4a2be5a4c0f6d985f2eb63964
PrinceRatna <arjnry125@gmail.com>
2021-09-02 06:21:04 +0600

    first commit1

--- js/api.js
+++ js/api.js
@@ -3,7 +3,7 @@ clickButton=()=>{
     const inputField=document.getElementById('input-field');
     const inputText=inputField.value;
     inputField.value='';
-    const url=`http://openlibrary.org/search.json?q=${inputText}`;
+    const url=`https://openlibrary.org/search.json?q=${inputText}`;
     fetch(url)
       .then(res=>res.json())
       .then(data=>displaySearchResult(data.docs));
@@ -16,7 +16,7 @@ displaySearchResult=docs=>{
     document.getElementById('count').innerHTML=`<h4 class='text-center'>No Result Found!!</h4> `;  
    }
    else{
-    document.getElementById('count').innerHTML=`<h4 class='text-center'>Search Results: ${docs.length}</h4> `;
+    document.getElementById('count').innerHTML=`<h4 class='text-center'> ${docs.length} Search Results Founded </h4> `;
     docs?.forEach(doc=>{
         const div=document.createElement('div');
         div.classList.add('col');
@@ -25,9 +25,9 @@ displaySearchResult=docs=>{
         <img src="https://covers.openlibrary.org/b/id/${doc.cover_i}-M.jpg" class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="card-title"><strong>Title:</strong>${doc.title}</h5>
-          <h6><strong>Author Name: </strong>${doc.author_name?doc.author_name:'no result found'}<br>
+          <strong>Author Name: </strong>${doc.author_name?doc.author_name:'no result found'}<br>
           <strong>Publisher: </strong>${doc.publisher?doc.publisher:'no result found'}<br>
-         <strong>First Publish Year: </strong>${doc.first_publish_year?doc.first_publish_year:'no result found'}</h6>
+         <strong>First Publish Year: </strong>${doc.first_publish_year?doc.first_publish_year:'no result found'}
         </div>
       </div>
         `
